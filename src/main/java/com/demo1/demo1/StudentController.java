f06db7fc6835621b82affeed583ec31d720d0348
tharindu432 <tharinduchaturanga432@gmail.com>
2023-08-17 17:16:07 +0530

    list of students(arraylist)-restapi

--- src/main/java/com/demo1/demo1/StudentController.java
+++ src/main/java/com/demo1/demo1/StudentController.java
@@ -3,13 +3,27 @@ package com.demo1.demo1;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class StudentController {
     @GetMapping("/student")
-    public Student getStudent(){
-        return new Student("tharindu","Ruwanpathirana");
+    public Student getStudent() {
+        return new Student("tharindu", "Ruwanpathirana");
 
     }
 
+    @GetMapping("/students")
+
+    public List<Student> getStudents() {
+        List<Student> students= new ArrayList<>();
+        students.add(new Student("tharindu", "Ruwanpathirana"));
+        students.add(new Student("tharindu", "Ruwanpathirana"));
+        students.add(new Student("tharindu", "Ruwanpathirana"));
+        return students;
+
+
+    }
 
 }
