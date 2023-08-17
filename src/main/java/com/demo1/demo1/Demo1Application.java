6001ad155af73757c08486579f0b3d5d0fb4193e
tharindu432 <tharinduchaturanga432@gmail.com>
2023-08-17 14:43:36 +0530

    @component annotations(beans)

--- src/main/java/com/demo1/demo1/Demo1Application.java
+++ src/main/java/com/demo1/demo1/Demo1Application.java
@@ -1,14 +1,17 @@
 package com.demo1.demo1;
 
+import com.demo1.demo1.sub1.StringClass;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Demo1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo1Application.class, args);
-		System.out.println("Hello World");
+		ConfigurableApplicationContext applicationContext= SpringApplication.run(Demo1Application.class, args);
+		StringClass component=applicationContext.getBean(StringClass.class);
+		//System.out.println("Hello World");
 	}
 
 }
---
+++ src/main/java/com/demo1/demo1/sub1/StringClass.java
@@ -0,0 +1,10 @@
+package com.demo1.demo1.sub1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StringClass {
+    public String getHelloWorld() {
+        return "Hello World";
+    }
+}
