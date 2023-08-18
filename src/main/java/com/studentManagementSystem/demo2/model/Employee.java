f45d3540fa43d54ab133d871d83b79862b936a20
tharindu432 <tharinduchaturanga432@gmail.com>
2023-08-18 17:49:55 +0530

    mssql database configuration & entity creation

---
+++ src/main/java/com/studentManagementSystem/demo2/model/Employee.java
@@ -0,0 +1,22 @@
+package com.studentManagementSystem.demo2.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "first_name",nullable = false)
+    private String firstName;
+    @Column(name = "last_name",nullable = false)
+    private String lastName;
+    @Column(name = "email",nullable = false)
+    private String email;
+
+
+}
