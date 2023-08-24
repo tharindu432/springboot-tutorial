857e44bac3202d0ff7993314938b41b3e8acd6e9
tharindu432 <tharinduchaturanga432@gmail.com>
2023-08-24 11:00:54 +0530

    create employee controlller developed

--- src/main/java/com/studentManagementSystem/demo2/Contoller/EmployeeController.java
+++ src/main/java/com/studentManagementSystem/demo2/Contoller/EmployeeController.java
@@ -1,11 +1,31 @@
 package com.studentManagementSystem.demo2.Contoller;
 
 import com.studentManagementSystem.demo2.Service.EmployeeService;
+import com.studentManagementSystem.demo2.model.Employee;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/employees")
 public class EmployeeController {
     private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService){
+        super();
+        this.employeeService=employeeService;
+    }
+
+    //build create employee REST API
+
+    @PostMapping()
+    public ResponseEntity<Employee> saveEmployee(@RequestBody  Employee employee){
+        return new ResponseEntity<>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+
+    }
 
 
 }
